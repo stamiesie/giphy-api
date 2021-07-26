0f4e391ae43f5af7eb8a23a1094785c71770ab99
Stephen Tamiesie <stephen.tamiesie@gmail.com>
2021-07-26 11:04:59 -0700

    setup fetch call

--- giphy-api/src/App.js
+++ giphy-api/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { GiphyFetch } from '@giphy/js-fetch-api';
+import {useState} from 'react';
+import TextList from './components/TextList';
+import Error from './components/Error';
 import './App.css';
 
+const giphy = new GiphyFetch(process.env.REACT_APP_GIPHY_KEY);
+
 function App() {
+  const [text, setText] = useState('');
+  const [results, setResults] = useState([]);
+  const [err, setErr] = useState(false);
+
+  const handleInput = (e) => {
+    setText(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    // error handling for empty submit, set error state to true
+    if(text.length === 0) {
+      setErr(true)
+      return
+    }
+    // if text length > 1, fetch from the api
+    console.log(text);
+
+    const fetchGifs = async () => {
+      const res = await giphy.animate(text, {limit: 20})
+      console.log(res.data);
+      setResults(res.data);
+    }
+    fetchGifs()
+    // reset text state back to empty string
+    setText('')
+    // reset error state back to false
+    setErr(false);
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Animated Text Generator</h1>
+      <input className='input-field' placeholder='type text here' value={text} onChange={handleInput}/>
+      <button className='submit-button' onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
